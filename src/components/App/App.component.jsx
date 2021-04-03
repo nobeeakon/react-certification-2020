010619b13f642d8fdc8ddd76fe67403ba06bc42f
DanielT <nobeeakon@gmail.com>
2021-04-02 19:09:09 -0700

    Included the new pages (Results and Watch)

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -8,11 +8,15 @@ import ThemesProvider from '../../providers/Theme';
 import NavBar from '../NavBar';
 
 import HomePage from '../../pages/Home';
+import WatchPage from '../../pages/Watch';
+import Results from '../../pages/Results';
 import LoginPage from '../../pages/Login';
 import NotFound from '../../pages/NotFound';
 import SecretPage from '../../pages/Secret';
 import Private from '../Private';
 import Layout from '../Layout';
+
+import { ROUTES } from '../../utils/functions/routes';
 
 function App() {
   return (
@@ -26,13 +30,20 @@ function App() {
                 <Route exact path="/">
                   <HomePage />
                 </Route>
+
+                <Route exact path={`/${ROUTES.RESULTS}`}>
+                  <Results />
+                </Route>
+                <Route exact path={`/${ROUTES.WATCH}`}>
+                  <WatchPage />
+                </Route>
                 <Route exact path="/login">
                   <LoginPage />
                 </Route>
                 <Private exact path="/secret">
                   <SecretPage />
                 </Private>
-
+                {/* TODO check if this is working correctly */}
                 <Route path="*">
                   <NotFound />
                 </Route>
