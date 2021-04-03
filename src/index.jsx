2b865050eb52525c2c65f078b49a6ebd026e5ee9
DanielT <nobeeakon@gmail.com>
2021-04-02 18:58:10 -0700

    Start Mock Service Worker when developing, to prevent multiple API calls due to quota limits. Forgot to add this into commit 172db5463b7e0521

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// mock service worker, for development only
+import { worker } from './mocks/browser';
+
 import App from './components/App';
 
 import GlobalStyle from './GlobalStyles';
+
+// mock service worker, for development only
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.render(
   <React.StrictMode>
