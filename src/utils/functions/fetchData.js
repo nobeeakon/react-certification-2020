578d3c37dd852276431a57b309519f3ce6e979b0
DanielT <nobeeakon@gmail.com>
2021-04-14 18:42:45 -0600

    added try-catch blocks to the each request

--- src/utils/functions/fetchData.js
+++ src/utils/functions/fetchData.js
@@ -15,44 +15,59 @@ const youtubeAPI = axios.create({
 // 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=[YOUR_API_KEY]'
 // TODO: maybe at some point add functionality to change type param (videos, playlists, channels)
 export const fetchVideoListByTerm = async (searchTerm) => {
-  const response = await youtubeAPI.get('/search', {
-    params: {
-      part: 'snippet',
-      maxResults: MAX_RESULTS,
-      q: searchTerm,
-      type: 'video',
-    },
-  });
+  try {
+    const response = await youtubeAPI.get('/search', {
+      params: {
+        part: 'snippet',
+        maxResults: MAX_RESULTS,
+        q: searchTerm,
+        type: 'video',
+      },
+    });
 
-  // filter out all non available videos
-  const itemsAvailable = response.data.items.filter(
-    (video) => video.id.videoId && video.snippet
-  );
-  return itemsAvailable;
+    // filter out all non available videos
+    const itemsAvailable = response.data.items.filter(
+      (video) => video.id.videoId && video.snippet
+    );
+    return itemsAvailable;
+  } catch (e) {
+    console.log(`Error while fetching videos related with the TERM: ${searchTerm}. `, e);
+    return null;
+  }
 };
 
 // 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=[YOUR_API_KEY]' \
 export const fetchRelatedVideos = async (videoId) => {
-  const response = await youtubeAPI.get('/search', {
-    params: {
-      part: 'snippet',
-      maxResults: MAX_RESULTS,
-      relatedToVideoId: videoId,
-      type: 'video',
-    },
-  });
+  try {
+    const response = await youtubeAPI.get('/search', {
+      params: {
+        part: 'snippet',
+        maxResults: MAX_RESULTS,
+        relatedToVideoId: videoId,
+        type: 'video',
+      },
+    });
 
-  return response.data.items;
+    return response.data.items;
+  } catch (e) {
+    console.log(`Error while fetching related videos, of the videoId: ${videoId}. `, e);
+    return null;
+  }
 };
 
 // https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=[YOUR_API_KEY]
 export const fetchVideoInfo = async (videoId) => {
-  const response = await youtubeAPI.get('/videos', {
-    params: {
-      part: ['snippet', 'contentDetails', 'statistics'].join(', '),
-      id: videoId,
-    },
-  });
+  try {
+    const response = await youtubeAPI.get('/videos', {
+      params: {
+        part: ['snippet', 'contentDetails', 'statistics'].join(', '),
+        id: videoId,
+      },
+    });
 
-  return response.data.items;
+    return response.data.items;
+  } catch (e) {
+    console.log(`Error while fetching video Info, of the videoId: ${videoId}. `, e);
+    return null;
+  }
 };
